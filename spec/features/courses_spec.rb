18ba8907b418ad55e6ba955ed422483cf334abab
henwanfan <henry.wang@berkeley.edu>
2025-04-18 11:19:37 -0700

    fixed rubocop

--- spec/features/courses_spec.rb
+++ spec/features/courses_spec.rb
@@ -27,22 +27,22 @@ RSpec.feature "Courses", type: :feature do
     )
     @student = FactoryBot.create :student, email: "google_student@berkeley.edu"
   end
-  
+
   scenario "user can view courses index page" do
     visit root_path
     click_button "Login with Google"
     expect(page).to have_link("Our Courses", href: courses_path)
     click_link "Our Courses"
-    
+
     # This checks that we've reached the courses page by its title
     expect(page).to have_content("Courses")
-    
+
     # This checks that we either see courses or the "no courses" message
-    expect(page).to satisfy do |p| 
-      p.has_css?('.card-header') || 
+    expect(page).to satisfy do |p|
+      p.has_css?(".card-header") ||
       p.has_content?("No courses are currently available")
     end
-    
+
     expect(current_path).to eq(courses_path)
   end
 end

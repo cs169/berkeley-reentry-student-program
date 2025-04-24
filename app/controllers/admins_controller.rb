b1508bf7effbedc191c63202a64b4400843d8cae
Sher Her <sherher21@gmail.com>
2025-04-24 11:23:57 -0700

    Updated checkin admin to use datatables

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,15 +7,7 @@ class AdminsController < ApplicationController
   def index; end
 
   def view_checkin_records
-    if (!params.key? :page) || (params[:page].to_i < 1)
-      redirect_to view_checkin_records_path(page: 1)
-      # return is needed here, otherwise the app will continue execute
-      # the following instructions after redirect
-      return
-    end
-    n = params[:page].to_i - 1
-    @checkin_records = Checkin.get_20_checkin_records(n)
-    @has_next_page = @checkin_records.size == 20
+    @checkin_records = Checkin.all
   end
 
   def manage_advisors

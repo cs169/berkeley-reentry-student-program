1e6e8e0a5a30c2df9c4197916df4b9bbc9e62c6b
WinbrosXP <jrich321@berkeley.edu>
2025-04-25 15:29:26 -0700

    tring button fixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,4 +10,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.5/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"

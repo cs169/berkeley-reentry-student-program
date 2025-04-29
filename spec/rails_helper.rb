c0db84a935d6deb2480bae9c263ba1d06e704489
Kefeng Duan <duankf@mail2.sysu.edu.cn>
2025-04-28 17:55:13 -0700

    Bug Fixes.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,9 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  # Set OmniAuth to test mode
+  OmniAuth.config.test_mode = true
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 

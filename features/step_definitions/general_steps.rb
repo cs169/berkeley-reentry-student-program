963b53bc01e8a048843e469239775b966b0f60cb
mandywong00 <mandywong@berkeley.edu>
2025-05-02 13:36:27 -0700

    rubocup fixes

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -13,11 +13,11 @@ Then(/^(?:|I )should not got "([^"]*)"$/) do |text|
 end
 
 Then(/^I leave "(.*?)" empty$/) do |field|
-  fill_in field, with: ''
+  fill_in field, with: ""
 end
 
 Then("I should receive a CSV file named with {string}") do |prefix|
-  disposition = page.response_headers['Content-Disposition']
+  disposition = page.response_headers["Content-Disposition"]
   expect(disposition).to include("attachment;")
   expect(disposition).to match(/filename="#{prefix}-\d{4}-\d{2}-\d{2}\.csv"/)
 end
